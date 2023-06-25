ed838e30d21c8b7897338bcf48ae8bb648e02b09
adamkhamismohammedabdeljali <adamkhamismohammedabdeljalil@gmail.com>
2023-06-25 12:49:42 +0200

    fix:exercise itIsPersonalNow

--- itIsPersonalNow/Program.cs
+++ itIsPersonalNow/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Please write your name:");
+string textName = Console.ReadLine();
+Console.WriteLine($"Hello, {textName}");
+Console.WriteLine("Hello, Please write your name:");
+//string textName = Console.ReadLine();
+Console.WriteLine($"Hello, {textName}");
